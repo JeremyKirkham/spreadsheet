fa610be77460e1804773162de759c8ce331491d2
Jeremy Kirkham <jeremy@streame.com.au>
2022-08-01 10:29:28 +1000

    chore: arrow navigation spec

--- cypress/e2e/sheet.cy.ts
+++ cypress/e2e/sheet.cy.ts
@@ -1,5 +1,5 @@
 describe("Spreadsheet", () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit("/");
   });
 
@@ -13,6 +13,24 @@ describe("Spreadsheet", () => {
     cy.get("#11").click();
     cy.get(".selectedCells").contains("A1");
     cy.get(".selectedCells").contains("A1");
+    cy.get("#header-A").should("have.class", "selected");
+    cy.get("#row-1").should("have.class", "selected");
+  });
+
+  it("navigates with arrows", () => {
+    cy.get("#11").click();
+    cy.get("#header-A").should("have.class", "selected");
+    cy.get("#row-1").should("have.class", "selected");
+    cy.get("body").type("{downArrow}");
+    cy.get("#header-A").should("have.class", "selected");
+    cy.get("#row-2").should("have.class", "selected");
+    cy.get("body").type("{rightArrow}");
+    cy.get("#header-B").should("have.class", "selected");
+    cy.get("#row-2").should("have.class", "selected");
+    cy.get("body").type("{upArrow}");
+    cy.get("#header-B").should("have.class", "selected");
+    cy.get("#row-1").should("have.class", "selected");
+    cy.get("body").type("{leftArrow}");
     cy.get("#header-A").should("have.class", "selected");
     cy.get("#row-1").should("have.class", "selected");
   });
