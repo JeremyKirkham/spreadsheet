e174b19f3e47497849b778d9aa295f4385d38311
Jeremy Kirkham <jeremy@streame.com.au>
2022-08-04 16:26:07 +1000

    chore: cleanup unused code

--- contexts/SelectedCellContext.tsx
+++ contexts/SelectedCellContext.tsx
@@ -3,42 +3,18 @@ import { useAppDispatch, useAppSelector } from "../hooks/store";
 import { xAndYToPos } from "../lib/xAndYtoPost";
 import { selectedCellPosition, update } from "../store/selectedCellSlice";
 
-interface Position {
-  x: number;
-  y: number;
-}
-
 interface SelectedCellContext {
-  highlightedRange?: {
-    start: Position;
-    end: Position;
-  };
-  setHighlightedRange: ({
-    start,
-    end,
-  }: {
-    start?: Position;
-    end?: Position;
-  }) => void;
-  mousedown: boolean;
   setInMenu: (inMenu: boolean) => void;
 }
 
 export const SelectedCellContext = createContext<SelectedCellContext>({
-  setHighlightedRange: () => {},
-  mousedown: false,
   setInMenu: () => {},
 });
 
 export const SelectedCellProvider: React.FC<PropsWithChildren<{}>> = ({
   children,
 }) => {
-  const [mousedown, setMousedown] = useState(false);
   const [inMenu, setInMenu] = useState(false);
-  const [highlightedStartX, setHighlightedStartX] = useState<number>();
-  const [highlightedStartY, setHighlightedStartY] = useState<number>();
-  const [highlightedEndX, setHighlightedEndX] = useState<number>();
-  const [highlightedEndY, setHighlightedEndY] = useState<number>();
   const cellPos = useAppSelector(selectedCellPosition);
   const dispatch = useAppDispatch();
 
@@ -103,31 +79,9 @@ export const SelectedCellProvider: React.FC<PropsWithChildren<{}>> = ({
     };
   }, [dispatch, cellPos, inMenu]);
 
-  const setHighlightedRange = ({
-    start,
-    end,
-  }: {
-    start?: Position;
-    end?: Position;
-  }) => {
-    if (start) {
-      setHighlightedStartX(start.x);
-      setHighlightedStartY(start.y);
-    }
-    if (end) {
-      setHighlightedEndX(end.x);
-      setHighlightedEndY(end.y);
-    }
-  };
-
-  const highlightedRange = undefined;
-
   return (
     <SelectedCellContext.Provider
       value={{
-        setHighlightedRange,
-        highlightedRange,
-        mousedown,
         setInMenu,
       }}
     >
